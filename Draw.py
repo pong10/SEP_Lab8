645e197146715477d0852abb52035e0d2aacb34a
pong10 <45727975+pong10@users.noreply.github.com>
2019-03-28 15:20:08 +0700

    Draw file

--- Draw.py
+++ Draw.py
@@ -4,9 +4,8 @@ from PySide2.QtWidgets import QGraphicsScene
 
 class Draw(QGraphicsScene):
     def __init__(self):
-        def __init__(self):
-            super(Draw, self).__init__()
-            self.color = [0,0,0]
+        super(Draw, self).__init__()
+        self.color = [0,0,0]
 
     def mouseMoveEvent(self, event:PySide2.QtWidgets.QGraphicsSceneMouseEvent):
         p = self.addEllipse(QCursor.pos().x(), QCursor.pos().y(), 20,20, QPen(QColor(self.color[0], self.color[1], self.color[2])),
