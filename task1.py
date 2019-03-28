2122c25e3bcbb69d7904fd44b30cc5eab7f736ee
somphon <45285645+somphonma@users.noreply.github.com>
2019-03-28 14:34:57 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -12,17 +12,11 @@ class draw_star(QWidget):
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
-
-
         p.setPen(QColor(65,105,225))
         p.setBrush(QColor(65,105,225))
-
-
-        p.drawPolygon([QPoint(0,85), QPoint(75,75), QPoint(100,10),QPoint(125,75), QPoint(200,85),
-                       QPoint(150,125),QPoint(160,190), QPoint(100,150), QPoint(40,190),QPoint(50,125), QPoint(0,85),])
-
-
-
+        #p.drawPolygon([QPoint(0,85), QPoint(75,75), QPoint(100,10),QPoint(125,75), QPoint(200,85),
+        #               QPoint(150,125),QPoint(160,190), QPoint(100,150), QPoint(40,190),QPoint(50,125), QPoint(0,85),])
+        p.drawPolygon([QPoint(100, 50), QPoint(50, 150), QPoint(150, 150), ])
         p.drawPixmap(QRect(200,100,320,320), self.rabbit)
         p.end()
 
