8f9a485ce5c74fa2f9c2901e5b53517a07549ef0
pong10 <45727975+pong10@users.noreply.github.com>
2019-03-28 15:43:58 +0700

    Update

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@ import turtle
 
 class Disk(object):
 
-    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
+    def __init__(self, name="", xpos=-100, ypos=0, height=20, width=40):
         self.dname = name
         self.dxpos = xpos
         self.dypos = ypos
@@ -70,3 +70,35 @@ def pushdisk(self,disk):
         self.toppos -= 1
         self.toppos -= d.dheight
         return d
+
+class Hanoi(object):
+
+    def __init__(self,n=3,start="A",workspace="B",destination="C"):
+        self.startp = Pole(start,0,0)
+        self.workspacep = Pole(workspace,150,0)
+        self.destinationp = Pole(destination,300,0)
+        self.startp.showpole()
+        self.workspacep.showpole()
+        self.destinationp.showpole()
+        for i in range(n):
+            self.startp.pushdisk(Disk("d"+str(i),0,i*150,20,(n-i)*30))
+
+    def move_disk(self,start,destination):
+        disk = start.popdisk()
+        destination.pushdisk(disk)
+
+    def move_tower(self,n,s,d,w):
+        if n == 1:
+            self.move_disk(s,d)
+        else:
+            self.move_tower(n-1,s,w,d)
+            self.move_disk(s,d)
+            self.move_tower(n-1,w,d,s)
+
+    def solve(self):
+        self.move_tower(3,self.startp,self.destinationp,self.workspacep)
+
+
+
+g = Hanoi()
+g.solve()
