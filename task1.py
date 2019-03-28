4b0b87a3c44cd76d039f27e2a6f8d91cb04691e8
pong10 <45727975+pong10@users.noreply.github.com>
2019-03-28 15:02:29 +0700

    f

--- task1.py
+++ task1.py
@@ -20,6 +20,15 @@ class Simple_drawing_window(QWidget):
         #p.drawPolygon([QPoint(0,85), QPoint(75,75), QPoint(100,10),QPoint(125,75), QPoint(200,85),
         #               QPoint(150,125),QPoint(160,190), QPoint(100,150), QPoint(40,190),QPoint(50,125), QPoint(0,85),])
         p.drawPolygon([QPoint(100, 50), QPoint(50, 150), QPoint(150, 150), ])
+
+        #Pong
+        p.setPen(QColor(239, 91, 69))
+        p.setBrush(QColor(252, 83, 58))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        p.drawPie(150, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([QPoint(50, 200), QPoint(250, 200), QPoint(150, 350), ])
+
         p.drawPixmap(QRect(200,100,320,320), self.rabbit)
         p.end()
 
