97a52b93b0fc20f3aed3471793b9f1d966b871e6
Nince <60090004@kmitl.ac.th>
2019-03-28 15:42:39 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -31,3 +31,42 @@ class Disk(object):
         turtle.pencolor("WHITE")
         self.showdisk()
         turtle.pencolor("BLACK")
+
+class Pole(object):
+    
+    def __init__(self,name="",xpos=0,ypos=0,thick=10,length=100):
+        self.pname = name
+        self.stack = []
+        self.toppos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+    
+    def showpole(self):
+        turtle.lt(90)
+        turtle.penup()
+        turtle.goto(self.pxpos,self.pypos)
+        turtle.pendown()
+        turtle.rt(90)
+        for x in range(2):
+            turtle.fd(self.pthick/2)
+            turtle.lt(90)
+            turtle.fd(self.plength)
+            turtle.lt(90)
+            turtle.fd(self.pthick/2)
+
+def pushdisk(self,disk):
+    disk.newpos(self.pxpos,self.toppos)
+    disk.showdisk()
+    self.stack.append(disk)
+    self.toppos += disk.dheight
+        self.toppos += 1
+    
+    
+    def popdisk(self):
+        d = self.stack.pop()
+        d.cleardisk()
+        self.toppos -= 1
+        self.toppos -= d.dheight
+        return d
