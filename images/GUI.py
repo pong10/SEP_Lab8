3cda669d3c41daf05548515d78fc35e3c09d153f
somphon <45285645+somphonma@users.noreply.github.com>
2019-03-28 15:15:01 +0700

    Gui

---
+++ images/GUI.py
@@ -0,0 +1,22 @@
+from PySide2 import QtCore,QtGui,QtWidgets
+class Ui_Form(object):
+    def setupUi(self,Form):
+        Form.setObjectName("Form")
+        Form.resize(723,572)
+        self.gridLayout=QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.drawField=QtWidgets.QtWidgets.QGraphicsView(Form)
+        self.drawField.setObjectName("drawField")
+        self.gridLayout.addWidget(self.drawField,0,0,1,1)
+        self.clear=QtWidgets.QPushButton(Form)
+        self.clear.setObjectName("clear")
+        self.gridLayout.addWidget(self.clear,2,0,1,1)
+        self.label=QtWidgets.QLabel(Form)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label,1,0,1,1)
+        self.retranslateUI(Form)
+        QtCore.QMetaObject.connectSlotsByname(Form)
+    def rectraslateUI(self,Form):
+        Form.setWindowTitle(QtWidgets.QApplication.translate("Form","Form",None,-1))
+        self.clear.setText(QtWidgets.QApplication.translate("Form","clear",None,-1))
+        self.label.setText(QtWidgets.QApplication.translate("Form","Drag the mouse to draw",None,-1))
